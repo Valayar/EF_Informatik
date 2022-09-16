5308c77c69be4b73b3359a857e466ea4d5d40893
Valayar <valentin.kuendig@edu.gbsl.ch>
2022-09-16 16:36:37 +0200

    Spielefeld zustand

---
+++ exercises/NumTrip Game/Spielfeld programmieren.py
@@ -0,0 +1,15 @@
+
+BREITE = 5
+LÄNGE = 5
+
+SPIELFELD = ( i for i in range(5))
+print(SPIELFELD)
+
+
+print("+-----+------+------+-----+-----+-----+\n"+  "I     I      I      I     I     I     I")
+print(SPIELFELD)
+
+
+
+
+

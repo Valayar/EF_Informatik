6be5a6fd9fd6099c575b5f7c18511d27c52e50db
Valayar <valentin.kuendig@edu.gbsl.ch>
2022-11-08 23:22:03 +0100

    Numbtrip Fertig

--- exercises/NumTrip Game/Spielfeld programmieren.py
+++ exercises/NumTrip Game/Spielfeld programmieren.py
@@ -3,6 +3,7 @@ init()
 init(autoreset=True)
 from colorama import Fore, Back
 
+Zeilenzähler = 0
 board = [
     [2, 4, 1, 8, 8],
     [4, 2, 8, 2, 1],
@@ -11,21 +12,25 @@ board = [
     [2, 4, 4, 4, 4]
 ]
 
+print( ' ',  end='' )
 for i in range(len(board[0])):
-    print( '', i, end='' )
-            
+    print( '', i, end='' )           
 print(' ')     
 
 
 for zeile in board:
+    Zeilenzähler = Zeilenzähler + 1
+    print(' ', end='')
     for zelle in zeile:
         print(Fore.GREEN + ' -', end='')
     print(' ')
+    print( Zeilenzähler, end='')
     for zelle in zeile:
         print(Fore.GREEN +f'|', end='')
         print(Back.RED +f'{zelle}', end='')
     print(Fore.GREEN + '|')
 
+print( ' ',  end='' )
 for zelle in board[0]:
     print(Fore.GREEN + ' -', end='')
 print(' ')

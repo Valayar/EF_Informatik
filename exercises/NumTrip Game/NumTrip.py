dd1e1c993c538cb429355ee3239e3c05b8ac3c83
Valayar <valentin.kuendig@edu.gbsl.ch>
2023-01-09 19:28:28 +0100

    add fieldupdate func to NumTrip game

--- exercises/NumTrip Game/NumTrip.py
+++ exercises/NumTrip Game/NumTrip.py
@@ -2,7 +2,7 @@ from colorama import init
 init()
 init(autoreset=True)
 from colorama import Fore, Back
-BUCHSTABENLISTE = ['A','B','C','D','E']
+Letter_List = ['A','B','C','D','E']
 board = [
     [2, 4, 1, 8, 8],
     [4, 2, 8, 2, 1],
@@ -11,7 +11,7 @@ board = [
     [2, 4, 4, 4, 4]
 ]
 def Feld_abrufen():
-    Zeilenzähler = 0
+    line_counter = 0
     print( ' ',  end='' )
     for i in range(len(board[0])):
         print( '', i+1, end='' )           
@@ -19,46 +19,45 @@ def Feld_abrufen():
 
 
 
-    for zeile in board:
+    for line in board:
         
         print(' ', end='')
-        for zelle in zeile:
+        for cell in line:
             print(Fore.GREEN + ' -', end='')
         print(' ')
-        print( BUCHSTABENLISTE[Zeilenzähler], end='')
-        for zelle in zeile:
+        print( Letter_List[line_counter], end='')
+        for cell in line:
             print(Fore.GREEN +f'|', end='')
-            print(Back.RED +f'{zelle}', end='')
+            print(Back.RED +f'{cell}', end='')
         print(Fore.GREEN + '|')
-        Zeilenzähler = Zeilenzähler + 1
+        line_counter = line_counter + 1
     print( ' ',  end='' )
-    for zelle in board[0]:
+    for cell in board[0]:
         print(Fore.GREEN + ' -', end='')
     print(' ')
 
 
-def input_check():
-        input_viable = False 
-        while input_viable == False:
+def Input_check():
+        Input_viable = False 
+        while Input_viable == False:
             Input = input('Geben sie das gewünschte feld ein (Format: A1 , a1)')
             Input = list(Input)
-            print(len(Input))
+            
             if len(Input) > 2:
                 print('Die Eingabe ist zu lang, versuchen sie es nochmals')
-                input_check()
+                continue
             if len(Input) < 2:
                 print('Die Eingabe ist zu kurz, versuchen sie es nochmals')
-                input_check()
+                continue
             if Input[0] in 'abcde':
-                BUCHSTABEN = ['A', 'B', 'C', 'D', 'E']
                 Input[0] = 'abcde'.index(Input[0])
-                Input[0] = BUCHSTABEN[Input[0]]
+                Input[0] = Letter_List[Input[0]]
             if Input[0] in 'ABCDE':
                 if Input[1] in '12345':
                     print('korrekte Eingabe')
                     global Input_Checked
                     Input_Checked = Input
-                    input_viable = True
+                    Input_viable = True
                 
                 else:
                     print('Zweite Ziffer ≠ 1, 2, 3, 4, 5  ')
@@ -69,9 +68,9 @@ def input_check():
 
 
 Input_Checked = []
-def Eingabe():
-    input_check()
-    print(Input_Checked)
+def User_Input():
+    Input_check()
+    
     Input_Checked[0] = 'ABCDE'.index(Input_Checked[0])
     Input_Checked[1] = int(Input_Checked[1])
     Input_Checked[1] = Input_Checked[1] - 1
@@ -80,12 +79,10 @@ def Eingabe():
     oldfieldvalue = board[Input_Checked[0]][Input_Checked[1]]
          
     def flood_fill(x ,y, old, new): 
-        print(x,y,old,new)
         if x < 0 or x >= len(board[0]) or y < 0 or y >= len(board):         
             return
         if board[x][y] != old:
             return
-        Feld_abrufen()
         board[x][y] = new
         flood_fill(x+1, y, old, new)
         flood_fill(x-1, y, old, new)
@@ -97,43 +94,36 @@ def Eingabe():
     board[Input_Checked[0]][Input_Checked[1]] = oldfieldvalue * 2
 
 
-'''
-def field_rearrange():
-    for line in board:
-        print (line)
-        for collum in line:
-            print (collum)
-            if collum == ' ':
-                print([collum])
-                board[1][1] = 1
 
-    print(board)
-'''   
-
-    
+import random
+random.seed(2)
+numbers = [1, 2, 4, 8]
+print(random.choice(numbers))
 
 
-
-
-
-
-
-
+def fieldrearrange():
+    for i in range(5):
+        for x in board:
+            for y in x :
+                if y == ' ' :
+                    if board.index(x) <= 0:
+                        board[0][x.index(y)] = random.choice(numbers)
+            
+                    if board.index(x) > 0:
+                        save = board[board.index(x) - 1][x.index(y)]
+                        board[board.index(x) - 1][x.index(y)] = ' '
+                        board[board.index(x)][x.index(y)] = save
+                        save = []
+                    
 
         
-
-
-    
-
-
-
 
 customfunc = True
 while customfunc == True :
     Feld_abrufen()
-    Eingabe()
-    Feld_abrufen()
-    '''field_rearrange()'''
+    User_Input()
+    fieldrearrange()
+    
     
     
 
